0b7930ff2c332f2d037bdfebe2ee8be1619a384d
Thomas Schuehly <thomas.schuehly@mimacom.com>
2023-07-10 23:42:44 +0200

    save

--- src/main/kotlin/de/tschuehly/microstreamtest/persistence/Root.kt
+++ src/main/kotlin/de/tschuehly/microstreamtest/persistence/Root.kt
@@ -1,19 +1,30 @@
 package de.tschuehly.microstreamtest.persistence
 
 import one.microstream.integrations.spring.boot.types.Storage
+import one.microstream.storage.types.StorageManager
+import org.springframework.beans.factory.annotation.Autowired
 import java.util.concurrent.atomic.AtomicLong
 
 @Storage
-class Root {
-    val persons = MutableMapStore<Person>()
+class Root{
+    @Autowired
+    @Transient
+    private lateinit var storageManager: StorageManager
+
+    val persons = MutableMapStore<Person>(storageManager)
 }
 
 class MutableMapStore<T>(
+    @Transient
+    private val storageManager: StorageManager
 ) {
     private var index: AtomicLong = AtomicLong(0)
     val map: MutableMap<Long, T> = mutableMapOf()
     fun newIndex(): Long {
         return index.incrementAndGet()
+    }
+    fun store(){
+        storageManager.store(this)
     }
 }
 
@@ -37,6 +48,7 @@ open class CrudRepository<T : Entity>(
         (obj.id ?: store.newIndex()).let { id ->
             obj.id = id
             store.map[id] = obj
+            store.store()
             return obj
         }
 
